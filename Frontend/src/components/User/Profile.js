538e41f333fb3ba8914dcbf72f6467b6bc1c55d3
Yi <jinyi06@gmail.com>
2021-02-23 23:25:10 -0800

    update user profile

--- Frontend/src/components/User/Profile.js
+++ Frontend/src/components/User/Profile.js
@@ -17,6 +17,7 @@ class Profile extends Component{
     error: '',
     fileSelected: '',
     saveStatus: null,
+    disabled: true,
   };
 
   componentDidMount(){
@@ -59,12 +60,17 @@ class Profile extends Component{
     try {
       const response = await Axios.post("/user/update", user)
       console.log("Profile saved: ", response.status);
-      this.setState({ saveStatus: true});
+      this.setState({ saveStatus: true, disabled: true});
       }
     catch (e) {
       console.log(e);
       this.setState({saveStatus: false});
     }
+  }
+
+  handleEditClick = () => {
+    const editStatus = !this.state.disabled;
+    this.setState({ disabled: editStatus});
   }
 
   render(){
@@ -83,15 +89,15 @@ class Profile extends Component{
             </div>
             <div class="col-md-4">
               <label for="name">Name:</label><br/>
-              <input class="form-control" type="text" id="name" name="name" value={this.state.user.name} readonly="readonly" onChange={this.handleChange}/><br/>
+              <input class="form-control" type="text" id="name" name="name" value={this.state.user.name} disabled={this.state.disabled} onChange={this.handleChange}/><br/>
               <label for="email">Email:</label><br/>
-              <input class="form-control" type="email" id="email" name="email" value={this.state.user.email} onChange={this.handleChange}/><br/>
+              <input class="form-control" type="email" id="email" name="email" value={this.state.user.email} disabled={this.state.disabled} onChange={this.handleChange}/><br/>
               <label for="phone">Phone Number:</label><br/>
-              <input class="form-control" type="tel" id="phone" name="phone" value={this.state.user.phone} onChange={this.handleChange}/><br/> 
+              <input class="form-control" type="tel" id="phone" name="phone" value={this.state.user.phone} disabled={this.state.disabled} onChange={this.handleChange}/><br/> 
             </div>
             <div class="col-md-4">
               <label for="currency">Default currency:</label><br/>
-              <select class="form-control" name="currency" id="currency" value={this.state.user.currency} onChange={this.handleChange}>
+              <select class="form-control" name="currency" id="currency" value={this.state.user.currency} disabled={this.state.disabled} onChange={this.handleChange}>
                 <option value="USD">USD</option>
                 <option value="KWD">KWD</option>
                 <option value="BHD">BHD</option>
@@ -100,7 +106,15 @@ class Profile extends Component{
                 <option value="CAD">CAD</option>
               </select><br/>
               <label for="timezone">Time zone:</label><br/>
+              <select class="form-control" name="timezone" id="timezone" value={this.state.user.timezone} disabled={this.state.disabled} onChange={this.handleChange}>
+                <option value="todo">ToDo</option>
+              </select><br/>               
               <label for="language">Language:</label><br/>
+              <select class="form-control" name="language" id="language" value={this.state.user.language} disabled={this.state.disabled} onChange={this.handleChange}>
+                <option value="English">English</option>
+              </select><br/>              
+              <input type = "button" value = "Edit" class="btn btn-secondary btn-lg" onClick={this.handleEditClick}/>
+              &nbsp;&nbsp;&nbsp;
               <input type = "submit" value = "Save" class="btn btn-success btn-lg"/>
             </div>
           </div>
